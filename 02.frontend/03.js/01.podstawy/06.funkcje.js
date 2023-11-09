4ec878531eebc55fe94c5e62d6eee29acf52d624
Jakub Sarnowski <hello@jsarnowski.com>
2023-11-09 19:41:09 +0100

    aktualizacja funkcji

--- 02.frontend/03.js/01.podstawy/06.funkcje.js
+++ 02.frontend/03.js/01.podstawy/06.funkcje.js
@@ -1,36 +1,49 @@
-// obiektowe, C#
-// public static ZrobCostam() {
-// 	
-// }
+/**
+ * FUNKCJE
+ *
+ * - moga miec input
+ * - moga miec output
+ *
+ * function test1(param1) {
+ * 					^- to jest input funkcji
+ * 		return 'something'
+ * 					^- to jest output funkcji (wartosc ktora funkcja zwraca)
+ * }
+ *
+ * const a = test1() // a = 'something'
+ *
+ * function test2() {
+ * 		return 'whatever'
+ * }
+ *
+ * const b = test2() // b = 'whatever'
+ *
+ * function test3() {
+ * 		console.log('asd')
+ * }
+ *
+ * const c = test3() // c = undefined
+ *
+ */
 
-// javascript
-
-// regular function
+// tworzenie funkcji (standardowa)
 function zrobCosTam(param1, param2) {
-	console.log(param1, param2)
+  console.log(param1, param2);
 }
 
-// zrobCosTam('hello', 'world')
+// wywolanie funkcji (standardowa)
+zrobCosTam("hello", "world");
 
-// arrow function
+// funkcja zwracająca wartość
+function stworzTablice() {
+  return [];
+}
 
+// tworzenie funkcji (strzalkowa - `arrow function`)
 const zrobCosTam = (param1, param2) => {
-	console.log(param1, param2)
-}
+  console.log(param1, param2);
+};
 
-// blueprint
-// function osoba(imie, nazwisko) {
-// 	this.imie = imie
-// 	this.nazwisko = nazwisko
-// }
-
-// const osoba1 = new osoba("wiktor", "robak")
-// console.log(osoba1)
-
-// const osoba2 = { imie: 'asd', nazwisko: 'zczxc' }
-
-// const osobaAleArrow = () => {
-// 	this.imie = imie
-// 	this.nazwisko = nazwisko
-// }
-
+// wywolanie funkcji (strzalkowa)
+// BEZ ROZNICY, TO SAMO
+zrobCosTam("asd1", "asd2");
